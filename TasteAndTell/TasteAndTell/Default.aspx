b65f9137dae96a01996aac352b40bb481ca05096
nthull <36854462+nthull@users.noreply.github.com>
2018-02-27 12:25:28 -0500

    Added Default.aspx, SearchSuggestion.aspx, web.sitemap, Taste.Master

---
+++ TasteAndTell/TasteAndTell/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Taste.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TasteAndTell.Pages.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Button ID="suggestLink" runat="server" Text="Recipe/Restaurant Suggestor" OnClick="suggestLink_Click"/>
+</asp:Content>
---
+++ TasteAndTell/TasteAndTell/Pages/SearchSuggestion.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace TasteAndTell.Pages
+{
+    public partial class SearchSuggestion : System.Web.UI.Page
+    {
+        private String recipeSuggestion;
+        private String restFoodSuggestion;
+        private String restLocSuggestion;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void RecipeSuggestButton_Click(object sender, EventArgs e)
+        {
+            recipeSuggestion = recipeSuggestor.Text.ToString();
+            String foodURL = "http://allrecipes.com/search/results/?wt=";
+            String foodURLEnd = "&sort=re";
+            String foodURLSearch = recipeSuggestion;
+            recipeSuggestor.Text = foodURL + foodURLSearch + foodURLEnd;
+            Response.Redirect(recipeSuggestor.Text);
+        }
+
+        protected void RestaurantSuggestorButton_Click(object sender, EventArgs e)
+        {
+            restLocSuggestion = restaurantLocSuggestor.Text.ToString();
+            restFoodSuggestion = restaurantFoodSuggestor.Text.ToString();
+            String restURL = "https://www.yelp.com/search?find_desc=&find_loc=";
+            String appendedURL = Regex.Replace(restURL, @"find_loc=", "find_loc=" + restLocSuggestion);
+            appendedURL = Regex.Replace(restURL, @"find_desc=", "find_desc=" + restFoodSuggestion);
+            Response.Redirect(appendedURL);
+        }
+    }
+}
