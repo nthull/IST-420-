c3b56ac266945772505716a8039b6a3cb2b43ee3
nthull <36854462+nthull@users.noreply.github.com>
2018-02-28 20:06:11 -0500

    Added SocialMedia for Anthony.

---
+++ TasteAndTell/TasteAndTell/Pages/SocialMedia.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Taste.Master" AutoEventWireup="true" CodeBehind="SocialMedia.aspx.cs" Inherits="TasteAndTell.Pages.SocialMedia" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ TasteAndTell/TasteAndTell/Pages/SocialMedia.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TasteAndTell.Pages
+{
+    public partial class SocialMedia : System.Web.UI.Page
+    {
+        
+        private string username;
+        private int Account_ID;
+        private string FirstName;
+        private string LastName;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ShowAdvertisement()
+        {
+
+        }
+
+        public void FacebookLogin()
+        {
+
+        }
+
+        public void TwitterLogin()
+        {
+
+        }
+
+        public void GoogleLogin()
+        {
+
+        }
+    }
+}
